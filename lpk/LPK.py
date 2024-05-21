7978279d0ea2478a2f66d724063b61c27ecaea97
Gokaze <alyamaisti@gmail.com>
2024-05-21 13:40:34 +0700

    Add files via upload

---
+++ lpk/LPK.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#judul aplikasi
+st.set_page_config(page_title="element tracker")
+st.subheader("Element **tracking**")
+st.warning("babi")
+st.caption("kami")
+st.selectbox("choose one",["kalkulator","tebak atom"])
+st.button("select this")
+st.tabs(1)
